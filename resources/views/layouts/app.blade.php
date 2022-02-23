800ba06d8db3c04485d4f6e2bf585defac7c7965
a.o <drealayodeji@icloud.com>
2022-02-23 03:20:30 +0000

    Updated with Gitro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}" />
     <link rel="icon" href="{{ asset('/img/eleosfund-white.png')}}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="antialiased">
 
@@ -17,27 +18,65 @@
         <div class="circleBackground w-[37rem] h-[37rem]"></div>
     </div>
 
-    <nav class="mx-5 flex flex-row justify-between items-center my-2 xl:my-10 xl:mx-32">
+    <nav class="mx-5 flex flex-row justify-between items-center my-2 xl:my-10 xl:mx-28">
 
         <div class="flex flex-row w-auto items-center h-[6vh] xl:h-[10vh] navLogo">
         <img src="{{ asset('/img/eleosfund.svg') }}" class="h-full" alt="logo" />
         <span class="font-medium text-xl xl:text-2xl mx-2">EleosFund</span>
         </div>
 
+        <div>
+        <button class="rounded-[50%] text-black p-3 xl:hidden" id="navBtn"><i class="fa-solid fa-bars text-2xl"></i></button>
+
         <div id="NavLinks" class="text-white font-medium hidden xl:block">
             <a  href="{{ route('about') }}" class="mx-3 hover:text-green-200 hover:underline hover:underline-offset-[8px]">About Us</a>
             <a  href="{{ route('contact') }}" class="mx-3 hover:text-green-200 hover:underline hover:underline-offset-[8px]">Contact Us</a>
             <a  href="{{ route('faqs') }}" class="mx-3 hover:text-green-200 hover:underline hover:underline-offset-[8px]">FAQs</a>
+            <a  href="https://www.twitter.com/eleosfund" class="mx-3 hover:text-green-200 hover:underline hover:underline-offset-[8px]" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i></a>
         </div>
+    </div>
         
     </nav>
+
+<div class="hidden" id="navMenuFull">
+<div class="w-[100vw] h-[100vh] bg-black opacity-30 absolute z-[-1] bottom-0 top-0 left-0 right-0" id="vignette"></div>
+<div class='absolute flex flex-col bg-white drop-shadow-lg w-1/2 h-[100vh] top-0 bottom-0 left-0 px-6 py-4 pt-16 opacity-100 font-light text-xl' id="navMenu">
+<a  href="{{ route('about')}}" class="pb-5 mb-5 border-b-2 border-zinc-300">About Us</a>
+<a  href="{{ route('about')}}" class="pb-5 mb-5 border-b-2 border-zinc-300">Contact Us</a>
+<a  href="{{ route('about')}}" class="pb-5 mb-5 border-b-2 border-zinc-300">FAQs</a>
+<a  href="https://www.twitter.com/eleosfund" class="pb-5 mb-5 border-b-2 border-zinc-300" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i> Twitter</a>
 </div>
+
+</div>
+</div>
+
 
     
 
     <section class="">
     @yield('content')
     </section>
+
+    <script type="text/javascript">
+    const navBtn = document.getElementById('navBtn')
+    const nav = document.getElementById('navMenuFull')
+    const vignette = document.getElementById('vignette')
+
+    navBtn.addEventListener('click', () => {
+        if(nav.classList.contains('hidden')){
+            nav.classList.remove('hidden')
+            navBtn.innerHTML = '<i class="fa-solid fa-xmark text-2xl"></i>'
+        } else {
+            nav.classList.add('hidden')
+            navBtn.innerHTML = '<i class="fa-solid fa-bars text-2xl">'
+        }
+        
+    })
+    vignette.addEventListener('click', () => {
+        nav.classList.add('hidden')
+        navBtn.innerHTML = '<i class="fa-solid fa-bars text-2xl">'
+    })
+    </script>
 
 </body>
 </html>
